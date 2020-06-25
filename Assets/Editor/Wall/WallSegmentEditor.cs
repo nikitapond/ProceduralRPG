3040a2f66290f0461f193d2411aa08e617825915
nikitapond <63856590+nikitapond@users.noreply.github.com>
2020-06-25 11:10:40 +0100

    Started work for procedurally generated 3d wall
    Basic straight wall, TODO - allow wall segments to follow a bezier curve

---
+++ Assets/Editor/Wall/WallSegmentEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(WallSegment)), CanEditMultipleObjects]
+public class WallSegmentEditor : Editor
+{
+    SerializedProperty wallLength;
+    SerializedProperty wallStart;
+    SerializedProperty wallHeight;
+
+    void OnEnable()
+    {
+        // Setup the SerializedProperties.
+        wallLength = serializedObject.FindProperty("WallLength");
+        wallStart = serializedObject.FindProperty("WallStart");
+        wallHeight = serializedObject.FindProperty("WallHeight");
+        //wallStart = serializedObject.FindProperty("armor");
+    }
+    public override void OnInspectorGUI()
+    {
+        // Update the serializedProperty - always do this in the beginning of OnInspectorGUI.
+        serializedObject.Update();
+
+        // Show the custom GUI controls.
+        EditorGUILayout.Slider(wallLength, 1, 100, new GUIContent("Wall Length"));
+        EditorGUILayout.Slider(wallHeight, 2, 5, new GUIContent("Wall Height"));
+
+        EditorGUILayout.PropertyField(wallStart);
+
+
+       
+
+  
+        // Apply changes to the serializedProperty - always do this in the end of OnInspectorGUI.
+        serializedObject.ApplyModifiedProperties();
+    }
+}
